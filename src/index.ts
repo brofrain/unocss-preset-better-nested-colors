c58c7e85f824c51e483fad4045ae0ddb355b5111
brofrain <downloading130@gmail.com>
2022-07-17 14:04:12 +0200

    use colors from preset param & add devtools

--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,51 @@
-import type {
-  Preset,
-  RuleContext,
-  DynamicShortcut,
-  UserShortcuts,
-} from "unocss";
-import type { AttributifyOptions } from "@unocss/preset-attributify";
+import { kebabize, isObj, isStr } from './helpers'
 
-import { kebabCase, isObj, isStr } from "./utils";
+import type { AttributifyOptions } from '@unocss/preset-attributify'
+import type { RuleContext, DynamicShortcut, Preset } from 'unocss'
 
 interface ThemeColors {
-  [key: string]: string | ThemeColors;
+  [key: string]: string | ThemeColors
 }
 
-interface UserTheme {
-  colors?: ThemeColors;
-  shortcuts?: UserShortcuts;
-}
-
-const THEME_COLORS_DEFAULT_KEY = "DEFAULT";
-const THEME_COLORS_VARIANT_KEY_PREFIX = ":";
-const THEME_COLORS_INHERIT_VALUE = "~";
-const REG_COMMON_CLASS_CHARS = "a-z 0-9 \\- \\_ \\, \\: \\[ \\] \\/ \\s";
+const THEME_COLORS_DEFAULT_KEY = 'DEFAULT'
+const THEME_COLORS_VARIANT_KEY_PREFIX = ':'
+const THEME_COLORS_PARENT_VALUE = '~'
+const REG_COMMON_CLASS_CHARS = 'a-z 0-9 \\- \\_ \\, \\: \\[ \\] \\/ \\s'
 
 const mergeKeys = (keyA: string, keyB: string) =>
-  kebabCase(`${keyA}-${keyB}`.replace("-:", ":")).replace(/-$/, "");
+  kebabize(
+    `${keyA}-${keyB}`.replace(
+      '-' + THEME_COLORS_VARIANT_KEY_PREFIX,
+      THEME_COLORS_VARIANT_KEY_PREFIX
+    )
+  ).replace(/-$/, '')
 
 const makeFlatColorsWithSuffix = (colors: ThemeColors) => {
-  const flatColors: Record<string, string> = {};
+  const flatColors: Record<string, string> = {}
 
   for (const [rawKey, value] of Object.entries(colors)) {
-    const key = rawKey === THEME_COLORS_DEFAULT_KEY ? "" : rawKey;
+    const key = rawKey === THEME_COLORS_DEFAULT_KEY ? '' : rawKey
 
     if (isStr(value)) {
-      flatColors[key] = value;
+      flatColors[key] = value
     } else if (isObj(value)) {
-      const flatValue = makeFlatColorsWithSuffix(value);
+      const flatValue = makeFlatColorsWithSuffix(value)
 
       for (const [subKey, subValue] of Object.entries(flatValue)) {
         if (
           key.startsWith(THEME_COLORS_VARIANT_KEY_PREFIX) &&
           !subKey.startsWith(THEME_COLORS_VARIANT_KEY_PREFIX)
         ) {
-          flatColors[mergeKeys(subKey, key)] = subValue;
+          flatColors[mergeKeys(subKey, key)] = subValue
         } else {
-          flatColors[mergeKeys(key, subKey)] = subValue;
+          flatColors[mergeKeys(key, subKey)] = subValue
         }
       }
     }
   }
 
-  return flatColors;
-};
+  return flatColors
+}
 
 const inheritColorFromParent = (
   colorsWithPrefix: Record<string, string>,
@@ -63,11 +58,11 @@ const inheritColorFromParent = (
       key.length > parentKey.length &&
       key.includes(parentKey) &&
       !key.endsWith(parentKey) // ignore adjacent variants
-  );
+  )
 
   if (!potentialParents.length) {
-    delete colorsWithPrefix[key];
-    return;
+    delete colorsWithPrefix[key]
+    return
   }
 
   const closestParent = potentialParents
@@ -75,27 +70,27 @@ const inheritColorFromParent = (
     .reduce(
       (keyA, keyB) => (keyA.length > keyB.length ? keyA : keyB),
       potentialParents[0]
-    );
+    )
 
-  colorsWithPrefix[key] = colorsWithPrefix[closestParent];
-};
+  colorsWithPrefix[key] = colorsWithPrefix[closestParent]
+}
 
 const setColorByReference = (
   colorsWithPrefix: Record<string, string>,
   colorsWithSuffix: Record<string, string>,
   key: string
 ) => {
-  const referenceKey = colorsWithPrefix[key];
+  const referenceKey = colorsWithPrefix[key]
 
   if (
     colorsWithPrefix[referenceKey] in colorsWithSuffix &&
     referenceKey !== colorsWithPrefix[referenceKey]
   ) {
-    setColorByReference(colorsWithPrefix, colorsWithSuffix, referenceKey);
+    setColorByReference(colorsWithPrefix, colorsWithSuffix, referenceKey)
   }
 
-  colorsWithPrefix[key] = colorsWithSuffix[referenceKey];
-};
+  colorsWithPrefix[key] = colorsWithSuffix[referenceKey]
+}
 
 const fillColorValues = (
   colorsWithPrefix: Record<string, string>,
@@ -103,11 +98,11 @@ const fillColorValues = (
 ) => {
   const prefixedKeys = Object.keys(colorsWithPrefix).sort(
     (keyA, keyB) => keyA.length - keyB.length
-  );
+  )
 
   for (const key of prefixedKeys) {
-    if (colorsWithPrefix[key] === THEME_COLORS_INHERIT_VALUE) {
-      inheritColorFromParent(colorsWithPrefix, prefixedKeys, key);
+    if (colorsWithPrefix[key] === THEME_COLORS_PARENT_VALUE) {
+      inheritColorFromParent(colorsWithPrefix, prefixedKeys, key)
     }
   }
 
@@ -116,167 +111,160 @@ const fillColorValues = (
       colorsWithPrefix[key] in colorsWithSuffix &&
       key !== colorsWithPrefix[key]
     ) {
-      setColorByReference(colorsWithPrefix, colorsWithSuffix, key);
+      setColorByReference(colorsWithPrefix, colorsWithSuffix, key)
     }
   }
-};
+}
 
 const makeFlatColorsWithPrefix = (colors: ThemeColors) => {
-  const colorsWithSuffix = makeFlatColorsWithSuffix(colors);
+  const colorsWithSuffix = makeFlatColorsWithSuffix(colors)
 
   const colorsWithPrefix = Object.fromEntries(
     Object.entries(colorsWithSuffix).map(([key, value]) => {
-      const keyArr = key.split(THEME_COLORS_VARIANT_KEY_PREFIX);
+      const keyArr = key.split(THEME_COLORS_VARIANT_KEY_PREFIX)
 
-      let newKey = keyArr.shift() as string;
+      let newKey = keyArr.shift() as string
 
       if (keyArr.length) {
         // has variants
-        keyArr.reverse();
-        newKey = `${keyArr.join(THEME_COLORS_VARIANT_KEY_PREFIX)}:${newKey}`;
+        keyArr.reverse()
+        newKey = `${keyArr.join(THEME_COLORS_VARIANT_KEY_PREFIX)}:${newKey}`
       }
 
-      return [newKey, value];
+      return [newKey, value]
     })
-  );
+  )
 
-  fillColorValues(colorsWithPrefix, colorsWithSuffix);
+  fillColorValues(colorsWithPrefix, colorsWithSuffix)
 
-  return colorsWithPrefix;
-};
+  return colorsWithPrefix
+}
 
 const makeDynamicColors = (colors: ThemeColors) => {
-  const flatColorsEntries = Object.entries(makeFlatColorsWithPrefix(colors));
+  const flatColorsEntries = Object.entries(makeFlatColorsWithPrefix(colors))
 
   return flatColorsEntries
     .map(([key, value]) => {
-      const keyArr: string[] = key.split(THEME_COLORS_VARIANT_KEY_PREFIX);
+      const keyArr: string[] = key.split(THEME_COLORS_VARIANT_KEY_PREFIX)
 
-      const colorName = keyArr.pop() as string;
+      const colorName = keyArr.pop() as string
 
       if (!keyArr.length) {
-        return { colorName, variantGroups: { ["" as string]: value } };
+        return { colorName, variantGroups: { ['' as string]: value } }
       }
 
-      keyArr.reverse();
-      const modifiers = (keyArr.join(":") + ":").toLowerCase();
+      keyArr.reverse()
+      const modifiers = (keyArr.join(':') + ':').toLowerCase()
 
-      return { colorName, variantGroups: { [modifiers]: value } };
+      return { colorName, variantGroups: { [modifiers]: value } }
     })
     .filter((item, i, arr) => {
       // merge variants of the same color name
 
       if (!i) {
-        return true;
+        return true
       }
 
       const previousItemOfSameName = arr
         .slice(0, i)
-        .find(({ colorName }) => colorName === item.colorName);
+        .find(({ colorName }) => colorName === item.colorName)
 
       if (!previousItemOfSameName) {
-        return true;
+        return true
       }
 
       const [variantGroup, variantGroupValue] = Object.entries(
         item.variantGroups
-      )[0];
+      )[0]
 
-      previousItemOfSameName.variantGroups[variantGroup] = variantGroupValue;
+      previousItemOfSameName.variantGroups[variantGroup] = variantGroupValue
 
-      return false;
-    });
-};
-
-let attributifyPrefix: string | null | undefined;
-
-const setAttributifyPrefix = ({ generator }: RuleContext) => {
-  const attributifyConfig = generator.config.presets
-    ?.flat()
-    ?.find(({ name }) => name === "@unocss/preset-attributify");
-
-  if (attributifyConfig) {
-    const { prefix } = attributifyConfig.options as AttributifyOptions;
-    attributifyPrefix = prefix || null;
-  } else {
-    attributifyPrefix = null;
-  }
-};
+      return false
+    })
+}
 
 const makeDynamicColorsShortcuts = (colors: ThemeColors) =>
   makeDynamicColors(colors).map(({ colorName, variantGroups }) => {
     const reg = new RegExp(
       [
-        "^",
-        `( [ ${REG_COMMON_CLASS_CHARS} \\( ] + )`,
-        "( [ ^ 0-9 a-z A-Z \\] \\/ ] )",
+        '^',
+        `( [ ${REG_COMMON_CLASS_CHARS} \\( ]+ )`, // preceding characters and eventual opening bracket of a variant group
+        '( [^  \\- \\s \\( ] [ \\- \\s \\( ] )', // dash OR opening bracket of a variant group OR space as a separator inside a variant group ONLY ONCE
         colorName,
-        "( ( \\] ) | ( \\/ \\d+ ) ) ?",
-        `( \\s ( [ ${REG_COMMON_CLASS_CHARS} ] + ) \\) ) ?`,
-        "$",
+        '( ( \\/ \\d+ ) )?', // optional opacity modifier
+        `( ( [ ${REG_COMMON_CLASS_CHARS} ]+ ) \\) ) ?`, // if closing bracket of a variant group, then some following characters are allowed
+        '$',
       ]
-        .join("")
-        .replaceAll(" ", "")
-    );
+        .join('')
+        .replaceAll(' ', '')
+    )
 
     const variantGroupsEntries = Object.entries(variantGroups).map(
       ([prefix, color]) => [
         prefix,
-        color.replaceAll(" ", "_"), // handle whitespaces
+        color.replaceAll(' ', '_'), // handle whitespaces
       ]
-    );
+    )
 
-    const ifArbitraryReg = new RegExp(`\\[(.*)${colorName}(.*)\\]`);
+    let attributifyPrefix: string | null | undefined // `null` - no Attributify Mode OR prefix is disabled | `undefined` - not checked for the prefix yet
+    const setAttributifyPrefix = ({ generator }: RuleContext) => {
+      const attributifyConfig = generator.config.presets.find(
+        ({ name }) => name === '@unocss/preset-attributify'
+      )
+
+      if (attributifyConfig) {
+        const { prefix } = attributifyConfig.options as AttributifyOptions
+        attributifyPrefix = prefix || null
+      } else {
+        attributifyPrefix = null
+      }
+    }
 
     return [
       reg,
       ({ input }: RegExpMatchArray, context: RuleContext) => {
+        if (!input) {
+          return ''
+        }
+
         if (attributifyPrefix === undefined) {
-          setAttributifyPrefix(context);
+          setAttributifyPrefix(context)
         }
 
         if (
           !input ||
-          input.startsWith(colorName) ||
-          input.startsWith("[" + colorName) ||
+          // don't generate redundant shortcuts for Attributify Mode
           (attributifyPrefix && input.startsWith(attributifyPrefix))
         ) {
-          return "";
+          return ''
         }
 
-        const isArbitrary = ifArbitraryReg.test(input);
+        const startsVariantGroup = input[0] === '('
 
-        return variantGroupsEntries
-          .map(
-            ([prefix, color]) =>
-              `${prefix}${input.replaceAll(
-                colorName,
-                isArbitrary ? color : `[${color}]`
-              )}`
-          )
-          .join(" ");
+        const utilityClass = startsVariantGroup ? input.slice(1) : input
+
+        return (
+          (startsVariantGroup ? '(' : '') +
+          variantGroupsEntries
+            .map(
+              ([prefix, color]) =>
+                `${prefix}${utilityClass.replaceAll(colorName, `[${color}]`)}`
+            )
+            .join(' ')
+        )
       },
-    ] as DynamicShortcut;
-  });
+    ] as DynamicShortcut
+  })
 
-const presetBetterNestedColors = () => {
-  const shortcuts: DynamicShortcut[] = [];
+export interface PresetBetterNestedColorsOptions {
+  colors: ThemeColors
+}
 
-  const preset: Preset<UserTheme> = {
-    name: "unocss-preset-colors-with-variants",
-    shortcuts,
-  };
+const presetBetterNestedColors = ({
+  colors,
+}: PresetBetterNestedColorsOptions): Preset => ({
+  name: 'unocss-better-nested-colors',
+  shortcuts: makeDynamicColorsShortcuts(colors),
+})
 
-  preset.extendTheme = (theme) => {
-    if (!theme.colors) {
-      return;
-    }
-
-    shortcuts.push(...makeDynamicColorsShortcuts(theme.colors));
-    theme.colors = {};
-  };
-
-  return preset;
-};
-
-export { presetBetterNestedColors };
+export { presetBetterNestedColors }
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   build: {
     lib: {
-      entry: "src/index.ts",
-      formats: ["es"],
-      fileName: () => "index.js",
+      entry: 'src/index.ts',
+      formats: ['es'],
+      fileName: () => 'index.js',
     },
     minify: false,
-    rollupOptions: { external: ["unocss"] },
-    target: "esnext",
+    rollupOptions: { external: ['unocss'] },
+    target: 'esnext',
   },
-});
+})
